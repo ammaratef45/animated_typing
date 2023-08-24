1091e0c86aa5e919a3d5ba44731782b12e840d1d
Ammar Hussein <ammar.atef45@gmail.com>
2023-08-23 17:40:03 -0700

    support animation on loop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.2.1] - 09/09/2020.
+
+* Added `loop` parameter to allow repeated animation
+
 ## [0.2.0] - 09/09/2020.
 
 * Upgrade SDK
--- lib/animated_typing.dart
+++ lib/animated_typing.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 
 /// [AnimatedTyping] widget will animate the showing of the passed [text].
 /// You can pass [style] and [duration] as well.
-class AnimatedTyping extends StatelessWidget {
+class AnimatedTyping extends StatefulWidget {
   /// The [text] shown in the widget.
   final String text;
 
@@ -15,6 +15,9 @@ class AnimatedTyping extends StatelessWidget {
   /// [duration] of the animation.
   final Duration? duration;
 
+  /// [loop] the animation (write and delete text forever)
+  final bool loop;
+
   /// Create a new widget passing [text], [style], and [duration]
   /// text can't be null.
   /// if [duration] is null a 3 seconds [duration] will be used
@@ -23,24 +26,38 @@ class AnimatedTyping extends StatelessWidget {
     required this.text,
     this.style,
     this.duration,
+    this.loop = false,
   }) : super(key: key);
+
+  @override
+  State<AnimatedTyping> createState() => _AnimatedTypingState();
+}
+
+class _AnimatedTypingState extends State<AnimatedTyping> {
+  String begin = '';
 
   /// Build the widget
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder<String>(
       tween: StringTween(
-        begin: '',
-        end: text,
+        begin: begin,
+        end: begin.isEmpty ? widget.text : '',
       ),
-      duration: duration ??
+      duration: widget.duration ??
           Duration(
             seconds: 3,
           ),
+      onEnd: () {
+        if (widget.loop)
+          setState(() {
+            begin = begin.isEmpty ? widget.text : '';
+          });
+      },
       builder: (context, value, child) {
         return Text(
           value,
-          style: style,
+          style: widget.style,
         );
       },
     );
